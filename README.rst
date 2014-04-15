c4f7e6b7b87cb836821cd386ed7cea5a8f8e079b
Mike Burns <mike@mike-burns.com>
2014-04-15 23:15:53 +0200

    Initial commit
    A skeleton of a project.

---
+++ README.rst
@@ -0,0 +1,14 @@
+===
+wim
+===
+
+Installation
+------------
+
+::
+
+    % python setup.py install
+
+Author
+------
+Copyright 2014 Mike Burns. Licensed under BSD 3-clause license.
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='wim',
+      version='0.1',
+      description='Keyboard controls for your WM',
+      long_description=readme(),
+      classifiers=[
+      ],
+      keywords='x wm repl shell',
+      url='http://github.com/mike-burns/wim',
+      author='Mike Burns',
+      author_email='mike@mike-burns.com',
+      license='BSD',
+      packages=['wim'],
+      entry_points={
+          'console_scripts': ['wimsh = wim.command_line:main'],
+      },
+      test_suite='nose.collector',
+      tests_require=['nose'])
